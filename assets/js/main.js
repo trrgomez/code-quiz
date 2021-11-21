ca6b8241b4b5aa4187188024fd0f0982cefa6201
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-11-21 13:49:08 -0600

    updated main.js file with comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,72 +23,76 @@ var penalty = 10;
 var highScores = [];
 
 // array of objects containing the questions and answers
-var questions = [
-    {
-      title: "Inside which HTML element do we put the JavaScript?",
-      choices: ["<scripting>", "<script>", "<js>", "<javascript>"],
-      answer: 1,
+var questions = [{
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<scripting>", "<script>", "<js>", "<javascript>"],
+        answer: 1,
     },
     {
-      title: "Where is the correct place to insert a JavaScript?",
-      choices: ["Both the <head> section and the <body> section are correct", "The <body> section", "The <head> section", "The <main> section"],
-      answer: 1,
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["Both the <head> section and the <body> section are correct", "The <body> section", "The <head> section", "The <main> section"],
+        answer: 1,
     },
     {
-      title: "Hyper Text Markup Language Stand For?",
-      choices: ["JavaScript", "XHTML", "CSS", "HTML"],
-      answer: 3,
+        title: "Hyper Text Markup Language Stand For?",
+        choices: ["JavaScript", "XHTML", "CSS", "HTML"],
+        answer: 3,
     },
     {
-      title: "What is the correct syntax for referring to an external script called xxx.js?",
-      choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script name='xxx.js'>", "<script ref='xxx.js'>"],
-      answer: 0,
+        title: "What is the correct syntax for referring to an external script called xxx.js?",
+        choices: ["<script src='xxx.js'>", "<script href='xxx.js'>", "<script name='xxx.js'>", "<script ref='xxx.js'>"],
+        answer: 0,
     },
     {
-      title: "What does CSS stand for",
-      choices: ["Coding Style Sheet", "Cascading Style Sheet", "CSS", "HTML"],
-      answer: 1,
+        title: "What does CSS stand for",
+        choices: ["Coding Style Sheet", "Cascading Style Sheet", "CSS", "HTML"],
+        answer: 1,
     },
     {
-      title: "How do you write 'Hello World' in an alert box?",
-      choices: ["alertBox('Hello World')", "alert('Hello World')", "msg('Hello World')", "msgBox('Hello World')"],
-      answer: 1,
+        title: "How do you write 'Hello World' in an alert box?",
+        choices: ["alertBox('Hello World')", "alert('Hello World')", "msg('Hello World')", "msgBox('Hello World')"],
+        answer: 1,
     },
     {
-      title: "How do you create a function in JavaScript?",
-      choices: ["function:myFunction()", "function myFunction()", "function = myFunction()", "function > myFunction()"],
-      answer: 1,
+        title: "How do you create a function in JavaScript?",
+        choices: ["function:myFunction()", "function myFunction()", "function = myFunction()", "function > myFunction()"],
+        answer: 1,
     },
-  ];
+];
 
-  startBtn.addEventListener("click", function () {
+//   will begin quiz when start button is clicked
+startBtn.addEventListener("click", function () {
+    //   decreases timer once quiz starts
     if (holdInterval === 0) {
-      holdInterval = setInterval(function () {
-        secondsLeft--;
-        currentTime.textContent = secondsLeft;
-        if (secondsLeft <= 0) {
-          clearInterval(holdInterval);
-          currentTime.textContent = "Time's up!";
-        }
-      }, 1000);
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = secondsLeft;
+            // if time reaches 0 then we clear interval and quiz will end
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                currentTime.textContent = "Your Time is Up!";
+            }
+        }, 1000);
     }
     startQuiz();
-  });
+});
 
-  var startQuiz = function () {
+
+var startQuiz = function () {
+    // hides the start page
     startPage.setAttribute("class", "hidden");
+    // shows quiz page
     quizPage.setAttribute("class", "active");
+    // function that will loop through questions array and display question and choices
     showQuestion(questions);
-  };
+};
 
-
-  var showQuestion = function (q) {
+// displays current question
+var showQuestion = function (question) {
     var divTitle = document.querySelector(".title");
-    divTitle.textContent = q[currentQuestion].title;
+    divTitle.textContent = question[currentQuestion].title;
     choicesEl.forEach((element, index) => {
-      element.textContent = q[currentQuestion].choices[index];
+        element.textContent = question[currentQuestion].choices[index];
     });
-  };
+};
 
-  
-  
